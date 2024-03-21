2159d459b74b470c2a5e449acde3edc62f12f7ab
dan khilkevich <danchuk213@gmail.com>
2024-03-21 18:21:12 +0200

    freed the array within the func instead of main and sorted the code

--- Prog_NivAlex_DanKhilkevich/program.c
+++ Prog_NivAlex_DanKhilkevich/program.c
@@ -25,12 +25,15 @@ int countLines(const char* filename)
 {
     char line[100];
     FILE* file_ptr = fopen(filename, "r");
+    
     if (file_ptr == NULL) {
         printf("Error, can not opening the file\n");
         return -1;
+    
     }
     int number_of_lines = 0; // we don't want to count the first line of the file. 
-    int ch;
+   int ch;
+
     while ((ch = fgetc(file_ptr)) != EOF)
     {
         if (ch == '\n')
@@ -52,7 +55,9 @@ void FromFile2Sportsman(const char* filename, sportsman* sportsmen_array[], int 
         printf("Error opening file %s\n", filename);
         exit(1);
     }
+
     *sportsmen_array = malloc(sportsmen_array_size * sizeof(sportsman));  // allocate memory for sportsmen array;
+
     if (*sportsmen_array == NULL) // allocate memory checking
     {
         printf("Memory allocation failed\n");
@@ -60,30 +65,39 @@ void FromFile2Sportsman(const char* filename, sportsman* sportsmen_array[], int 
     }
 
     char line[100];
+
     fgets(line, sizeof(line), file);  // skip the format line (the first line)
-    int count = 0;
-    while (fgets(line, sizeof(line), file) != NULL && count < sportsmen_array_size)  //read line by line from the file 
+    int sportmen_count = 0;
+
+    while (fgets(line, sizeof(line), file) != NULL && sportmen_count < sportsmen_array_size)  //read line by line from the file 
     {
         int gen;
         char clubName[100]; // temporary buffer for club name, club name = maximum of 100 chars
 
-        if (sscanf(line, "%d;%[^;];%[^;];%[^;];%d", &((*sportsmen_array)[count].id),(*sportsmen_array)[count].Fname,
-            (*sportsmen_array)[count].Lname,clubName,&gen) != 5) 
+        if (sscanf(line, "%d;%[^;];%[^;];%[^;];%d", &((*sportsmen_array)[sportmen_count].id), (*sportsmen_array)[sportmen_count].Fname,
+            (*sportsmen_array)[sportmen_count].Lname, clubName, &gen) != 5)
         {
             printf("Error reading data from file\n");
             exit(1);
         }
-        (*sportsmen_array)[count].gen = gen;
+        (*sportsmen_array)[sportmen_count].gen = gen;
 
         // dynamically allocate memory to p2club based on the length of clubName
-        (*sportsmen_array)[count].p2club = malloc((strlen(clubName) + 1) * sizeof(char));
-        if ((*sportsmen_array)[count].p2club == NULL) 
+        (*sportsmen_array)[sportmen_count].p2club = malloc((strlen(clubName) + 1) * sizeof(char));
+        if ((*sportsmen_array)[sportmen_count].p2club == NULL)
         {
             printf("Memory allocation failed\n");
             exit(1);
         }
-        strcpy((*sportsmen_array)[count].p2club, clubName);
-        count++;
+        strcpy((*sportsmen_array)[sportmen_count].p2club, clubName);
+
+        /*printf("ID: %d; Name: %s %s; Club: %s; Gender: %s\n", (*sportsmen_array)[sportmen_count].id,
+            (*sportsmen_array)[sportmen_count].Fname, (*sportsmen_array)[sportmen_count].Lname, (*sportsmen_array)[sportmen_count].p2club,
+            ((*sportsmen_array)[sportmen_count].gen == 1) ? "Male" : "Female");*/
+
+        free((*sportsmen_array)[sportmen_count].p2club);
+
+        sportmen_count++;
     }
 
     fclose(file);
@@ -91,18 +105,12 @@ void FromFile2Sportsman(const char* filename, sportsman* sportsmen_array[], int 
 
 void main() {
     int numSportsmen = countLines("SportsmanData.txt");
-    printf("number of sportsmen in the file %d\n", numSportsmen);
-    sportsman* sportsmen = NULL; //pointer to sportsmen array
-    FromFile2Sportsman("SportsmanData.txt", &sportsmen, numSportsmen);
-    printf("Sportsman Data:\n");
-    for (int i = 0; i < numSportsmen; i++) 
-    {
-        printf("ID: %d; Name: %s %s; Club: %s; Gender: %s\n", sportsmen[i].id,
-            sportsmen[i].Fname, sportsmen[i].Lname, sportsmen[i].p2club,
-            (sportsmen[i].gen == 1) ? "Male" : "Female");
 
-       
-        free(sportsmen[i].p2club); // free memory allocated for p2club
-    }
+    printf("number of sportsmen in the file %d\n", numSportsmen);
+
+    sportsman* sportsmen = NULL; //pointer to sportsmen array
+
+    FromFile2Sportsman("SportsmanData.txt", &sportsmen, numSportsmen);
+   
     free(sportsmen);  // free allocated memory for sportsmen array
 }
